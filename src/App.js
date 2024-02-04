5ca3ebb1ebc4aa5bed04eddbc18d5f2c131911db
avalsonigara15 <avals1504@gmail.com>
2024-02-04 12:39:58 +0530

    ~LOGIN

--- src/App.js
+++ src/App.js
@@ -14,315 +14,331 @@ import Auth from "./components/auth/Auth";
 import { useAuth } from "./context/AuthProvider";
 
 function App() {
-	const [textEditor, setTextEditor] = useState("input");
-	const [processing, setProcessing] = useState(false);
-	const [percentageStage, setPercentageStage] = useState(0);
-	const [selected, setSelected] = useState("python");
-	const [input, setInput] = useState("");
-	const [output, setOutput] = useState("");
-	const [python, setpython] = useState("");
-	const [cpp, setcpp] = useState("");
-	const [java, setjava] = useState("");
-	const [js, setjs] = useState("");
-	const [pascal, setpascal] = useState("");
-	const [perl, setperl] = useState("");
-	const [php, setphp] = useState("");
-	const [ruby, setruby] = useState("");
-	const [modal, setModal] = useState(false);
-	const [docId, setDocId] = useState(null);
-	const [isDocId, setIsDocId] = useState(false);
-	const user = localStorage.getItem("user");
-	const { isAuthenticated } = useAuth();
-	const [isInputBoxShown, setisInputBoxShown] = useState(true);
+  const [textEditor, setTextEditor] = useState("input");
+  const [processing, setProcessing] = useState(false);
+  const [percentageStage, setPercentageStage] = useState(0);
+  const [selected, setSelected] = useState("python");
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const [python, setpython] = useState("");
+  const [cpp, setcpp] = useState("");
+  const [java, setjava] = useState("");
+  const [js, setjs] = useState("");
+  const [pascal, setpascal] = useState("");
+  const [perl, setperl] = useState("");
+  const [php, setphp] = useState("");
+  const [ruby, setruby] = useState("");
+  const [modal, setModal] = useState(false);
+  const [docId, setDocId] = useState(null);
+  const [isDocId, setIsDocId] = useState(false);
+  const user = localStorage.getItem("user");
+  const { isAuthenticated } = useAuth();
+  const [isInputBoxShown, setisInputBoxShown] = useState(true);
 
-	useEffect(() => {
-		if (window.location.pathname === "/") {
-			const uid = uuidV4();
-			setDocId(uid);
-			setIsDocId(false);
-		} else {
-			setDocId(window.location.pathname.split("/")[1]);
-			setIsDocId(true);
-		}
-		if (isAuthenticated) {
-			ReactGA.event({
-				category: `user.logged`,
-				action: `Login`,
-				label: `${user.email}`,
-			});
-		}
-		// eslint-disable-next-line
-	}, []);
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      const uid = uuidV4();
+      setDocId(uid);
+      setIsDocId(false);
+    } else {
+      setDocId(window.location.pathname.split("/")[1]);
+      setIsDocId(true);
+    }
+    if (isAuthenticated) {
+      ReactGA.event({
+        category: `user.logged`,
+        action: `Login`,
+        label: `${user.email}`,
+      });
+    }
+    // eslint-disable-next-line
+  }, []);
 
-	let statusLoop = null;
+  let statusLoop = null;
 
-	const runCode = () => {
-		ReactGA.event({
-			category: `button.clicked`,
-			action: `Run Code`,
-			lang: `${selected}`,
-		});
+  const runCode = () => {
+    ReactGA.event({
+      category: `button.clicked`,
+      action: `Run Code`,
+      lang: `${selected}`,
+    });
 
-		setOutput("");
-		setTextEditor("output");
-		setProcessing(true);
-		setPercentageStage(10);
-		setisInputBoxShown(false);
+    setOutput("");
+    setTextEditor("output");
+    setProcessing(true);
+    setPercentageStage(10);
+    setisInputBoxShown(false);
 
-		var lang = selected;
-		const backend_url = process.env.REACT_APP_BACKEND_ENDPOINT_URL + "/runcode";
-		var source = "print(1)";
-		if (lang === "python") {
-			source = python;
-		} else if (lang === "cpp") {
-			source = cpp;
-		} else if (lang === "java") {
-			source = java;
-		} else if (lang === "javascript") {
-			source = js;
-		} else if (lang === "pascal") {
-			source = pascal;
-		} else if (lang === "perl") {
-			source = perl;
-		} else if (lang === "php") {
-			source = php;
-		} else if (lang === "ruby") {
-			source = ruby;
-		}
-		if (lang === "javascript") lang = "javascript_node";
-		var data = {
-			lang: lang.toUpperCase(),
-			source: source,
-			input: input,
-			memory_limit: 243232,
-			time_limit: 5,
-			context: "{'id': 213121}",
-			callback: "https://client.com/callback/",
-		};
+    var lang = selected;
+    const backend_url = process.env.REACT_APP_BACKEND_ENDPOINT_URL + "/runcode";
+    var source = "print(1)";
+    if (lang === "python") {
+      source = python;
+    } else if (lang === "cpp") {
+      source = cpp;
+    } else if (lang === "java") {
+      source = java;
+    } else if (lang === "javascript") {
+      source = js;
+    } else if (lang === "pascal") {
+      source = pascal;
+    } else if (lang === "perl") {
+      source = perl;
+    } else if (lang === "php") {
+      source = php;
+    } else if (lang === "ruby") {
+      source = ruby;
+    }
+    if (lang === "javascript") lang = "javascript_node";
+    var data = {
+      lang: lang.toUpperCase(),
+      source: source,
+      input: input,
+      memory_limit: 243232,
+      time_limit: 5,
+      context: "{'id': 213121}",
+      callback: "https://client.com/callback/",
+    };
 
-		var status;
-		var raw = JSON.stringify(data);
+    var status;
+    var raw = JSON.stringify(data);
 
-		var requestOptions = {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: raw,
-			redirect: "follow",
-		};
-		fetch(backend_url, requestOptions)
-			.then((res) => res.json())
-			.then((data) => {
-				status = data.status_update_url;
-				const url = backend_url + "?url=" + status;
+    var requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: raw,
+      redirect: "follow",
+    };
+    fetch(backend_url, requestOptions)
+      .then((res) => res.json())
+      .then((data) => {
+        status = data.status_update_url;
+        const url = backend_url + "?url=" + status;
 
-				setPercentageStage(25);
+        setPercentageStage(25);
 
-				statusLoop = setInterval(() => {
-					fetch(url, {
-						method: "GET",
-					})
-						.then((res) => res.json())
-						.then((data) => {
-							setPercentageStage(75);
-							// console.log(data);
-							if (data.result.compile_status === "OK") {
-								if (data.result.run_status.status === "AC") {
-									getOutput(data.result.run_status.output);
-									clearInterval(statusLoop);
-								} else if (data.result.run_status.status === "OLE") {
-									setOutput(data.result.run_status.status_detail);
-									setProcessing(false);
-									clearInterval(statusLoop);
-								} else if (data.result.run_status.status !== "NA") {
-									setOutput(data.result.run_status.stderr);
-									setProcessing(false);
-									clearInterval(statusLoop);
-								}
-							} else {
-								setOutput(data.result.compile_status);
-								setProcessing(false);
-								clearInterval(statusLoop);
-								return;
-							}
-						})
-						.catch((e) => {
-							setProcessing(false);
-							clearInterval(statusLoop);
-							console.log(e);
-						});
-				}, 2000);
-			})
-			.catch((e) => {
-				console.log(e);
-			});
-	};
+        statusLoop = setInterval(() => {
+          fetch(url, {
+            method: "GET",
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              setPercentageStage(75);
+              // console.log(data);
+              if (data.result.compile_status === "OK") {
+                if (data.result.run_status.status === "AC") {
+                  getOutput(data.result.run_status.output);
+                  clearInterval(statusLoop);
+                } else if (data.result.run_status.status === "OLE") {
+                  setOutput(data.result.run_status.status_detail);
+                  setProcessing(false);
+                  clearInterval(statusLoop);
+                } else if (data.result.run_status.status !== "NA") {
+                  setOutput(data.result.run_status.stderr);
+                  setProcessing(false);
+                  clearInterval(statusLoop);
+                }
+              } else {
+                setOutput(data.result.compile_status);
+                setProcessing(false);
+                clearInterval(statusLoop);
+                return;
+              }
+            })
+            .catch((e) => {
+              setProcessing(false);
+              clearInterval(statusLoop);
+              console.log(e);
+            });
+        }, 2000);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-	const getOutput = (link) => {
-		axios
-			.get(link)
-			.then((res) => {
-				setPercentageStage(100);
-				setProcessing(false);
-				setOutput(res.data);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
+  const getOutput = (link) => {
+    axios
+      .get(link)
+      .then((res) => {
+        setPercentageStage(100);
+        setProcessing(false);
+        setOutput(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-	const toggleModal = () => {
-		ReactGA.event({
-			category: `button.clicked`,
-			action: `Whiteboard ${modal ? "Opened" : "Closed"}`,
-		});
-		setModal(!modal);
-	};
+  const toggleModal = () => {
+    ReactGA.event({
+      category: `button.clicked`,
+      action: `Whiteboard ${modal ? "Opened" : "Closed"}`,
+    });
+    setModal(!modal);
+  };
 
-	return isAuthenticated ? (
-		<div className="h-screen bg-primary-5 flex flex-grow flex-col">
-			{isDocId ? (
-				<>
-					<Header
-						userInfo={user}
-						runCode={runCode}
-						isAuthenticated={isAuthenticated}
-						toggleModal={toggleModal}
-						isInputBoxShown={isInputBoxShown}
-						setisInputBoxShown={setisInputBoxShown}
-					/>
-					<IDE
-						docId={docId}
-						modal={modal}
-						java={java}
-						setjava={setjava}
-						toggleModal={toggleModal}
-						setModal={setModal}
-						cpp={cpp}
-						setcpp={setcpp}
-						js={js}
-						setjs={setjs}
-						php={php}
-						setphp={setphp}
-						perl={perl}
-						setperl={setperl}
-						ruby={ruby}
-						setruby={setruby}
-						pascal={pascal}
-						setpascal={setpascal}
-						python={python}
-						setpython={setpython}
-						input={input}
-						setInput={setInput}
-						selected={selected}
-						setSelected={setSelected}
-						output={output}
-						setOutput={setOutput}
-						textEditor={textEditor}
-						setTextEditor={setTextEditor}
-						processing={processing}
-						setProcessing={setProcessing}
-						percentageStage={percentageStage}
-						setPercentageStage={setPercentageStage}
-						isInputBoxShown={isInputBoxShown}
-					/>
-				</>
-			) : (
-				<Preview docId={docId} />
-			)}
-		</div>
-	) : (
-		<Auth />
-	);
+  return isAuthenticated ? (
+    <div className="h-screen bg-primary-5 flex flex-grow flex-col">
+      {isDocId ? (
+        <>
+          <Header
+            userInfo={user}
+            runCode={runCode}
+            isAuthenticated={isAuthenticated}
+            toggleModal={toggleModal}
+            isInputBoxShown={isInputBoxShown}
+            setisInputBoxShown={setisInputBoxShown}
+          />
+          <IDE
+            docId={docId}
+            modal={modal}
+            java={java}
+            setjava={setjava}
+            toggleModal={toggleModal}
+            setModal={setModal}
+            cpp={cpp}
+            setcpp={setcpp}
+            js={js}
+            setjs={setjs}
+            php={php}
+            setphp={setphp}
+            perl={perl}
+            setperl={setperl}
+            ruby={ruby}
+            setruby={setruby}
+            pascal={pascal}
+            setpascal={setpascal}
+            python={python}
+            setpython={setpython}
+            input={input}
+            setInput={setInput}
+            selected={selected}
+            setSelected={setSelected}
+            output={output}
+            setOutput={setOutput}
+            textEditor={textEditor}
+            setTextEditor={setTextEditor}
+            processing={processing}
+            setProcessing={setProcessing}
+            percentageStage={percentageStage}
+            setPercentageStage={setPercentageStage}
+            isInputBoxShown={isInputBoxShown}
+          />
+        </>
+      ) : (
+        <Preview docId={docId} />
+      )}
+    </div>
+  ) : (
+    <Auth />
+  );
 }
 
 export default App;
 
-function Header({ runCode, toggleModal, isAuthenticated, isInputBoxShown, setisInputBoxShown }) {
-	const [toolTip, showToolTip] = useState(false);
-	const [userInfo, setUserInfo] = useState({});
-	const [isUserPresent, setIsUserPresent] = useState(false);
+function Header({
+  runCode,
+  toggleModal,
+  isAuthenticated,
+  isInputBoxShown,
+  setisInputBoxShown,
+}) {
+  const [toolTip, showToolTip] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
+  const [isUserPresent, setIsUserPresent] = useState(false);
 
-	useEffect(() => {
-		try {
-			const user = JSON.parse(localStorage.getItem("user"));
-			setUserInfo(user);
-			if (user) {
-				setIsUserPresent(true);
-			} else setIsUserPresent(false);
-		} catch (e) {
-			setIsUserPresent(false);
-		}
-	}, []);
+  useEffect(() => {
+    try {
+      const user = JSON.parse(localStorage.getItem("user"));
+      setUserInfo(user);
+      if (user) {
+        setIsUserPresent(true);
+      } else setIsUserPresent(false);
+    } catch (e) {
+      setIsUserPresent(false);
+    }
+  }, []);
 
-	const toggleInputBox = () => {
-		ReactGA.event({
-			category: `button.clicked`,
-			action: `Input Box ${isInputBoxShown ? "Closed" : "Opened"}`,
-		});
-		setisInputBoxShown(!isInputBoxShown);
-	};
+  const toggleInputBox = () => {
+    ReactGA.event({
+      category: `button.clicked`,
+      action: `Input Box ${isInputBoxShown ? "Closed" : "Opened"}`,
+    });
+    setisInputBoxShown(!isInputBoxShown);
+  };
 
-	return (
-		<div className=" bg-primary-2 flex py-2 px-4 justify-between items-center rounded-b-lg custom-shadow-medium">
-			<div className="flex items-center">
-				<div className="h-7 flex items-center font-medium text-xl codeFont text-orange-standard">
-					{/* <img className="h-full" src={"./logo.png"} alt="codeconnect logo" /> */}
-					<span className="ml-2 text-primary-5">&#123; ..SyncLink.. &#125;</span>
-				</div>
-			</div>
-			<div className="flex items-center">
-				<Tooltip label="Input/Output" hasArrow fontSize="md" bg="#FFF0F3" textColor="#590D22">
-					<button className=" text-primary-5 mr-4" onClick={toggleInputBox}>
-						<Icon icon="bi:input-cursor-text" className="text-orange-standard" height="24" />
-					</button>
-				</Tooltip>
-				<Tooltip label="Whiteboard" hasArrow fontSize="md" bg="#FFF0F3" textColor="#590D22">
-					<button className=" text-primary-5 mr-4" onClick={toggleModal}>
-						<Icon icon={whiteboard24Regular} className="text-orange-standard" height="28" />
-					</button>
-				</Tooltip>
-				<Tooltip label="Run Code" hasArrow fontSize="sm" bg="#FFF0F3" textColor="#590D22">
-					<button
-						onClick={runCode}
-						className="bg-primary-5 flex items-center text-primary-1 font-medium rounded px-3 py-0.5 mr-2"
-					>
-						<img className="h-2.5" src={runIcon} alt="run code icon" />
-						<span className="ml-2">Run</span>
-					</button>
-				</Tooltip>
-				{/* {
-          isAuthenticated ?
-            <Logout /> :
-            <Login />
-        } */}
-				{isUserPresent && (
-					<Tooltip label={userInfo.email} hasArrow fontSize="sm" bg="teal.900">
-						<Avatar size="sm" name={userInfo.name} src={userInfo.picture} />
-					</Tooltip>
-				)}
-				<div className="mx-1 relative">
-					{isAuthenticated && (
-						<img
-							onMouseEnter={() => {
-								showToolTip(true);
-							}}
-							onMouseLeave={() => {
-								showToolTip(false);
-							}}
-							className="h-7 w-7 rounded-full"
-							src={userInfo.picture}
-							alt="user icon"
-						/>
-					)}
-					{toolTip && isAuthenticated && (
-						<div className="absolute z-50 top-full right-0 mt-2 text-center text-xs text-gray-200 bg-black mr-4 px-1">
-							{userInfo.email}
-						</div>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className=" bg-primary-2 flex py-2 px-4 justify-between items-center rounded-b-lg custom-shadow-medium">
+      <div className="flex items-center">
+        <div className="h-7 flex items-center font-medium text-xl codeFont text-orange-standard">
+          {/* <img className="h-full" src={"./logo.png"} alt="codeconnect logo" /> */}
+          <span className="ml-2 text-primary-5">
+            &#123; ..SyncLink.. &#125;
+          </span>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <Tooltip
+          label="Input/Output"
+          hasArrow
+          fontSize="md"
+          bg="#FFF0F3"
+          textColor="#590D22"
+        >
+          <button className=" text-primary-5 mr-4" onClick={toggleInputBox}>
+            <Icon
+              icon="bi:input-cursor-text"
+              className="text-orange-standard"
+              height="24"
+            />
+          </button>
+        </Tooltip>
+        <Tooltip
+          label="Whiteboard"
+          hasArrow
+          fontSize="md"
+          bg="#FFF0F3"
+          textColor="#590D22"
+        >
+          <button className=" text-primary-5 mr-4" onClick={toggleModal}>
+            <Icon
+              icon={whiteboard24Regular}
+              className="text-orange-standard"
+              height="28"
+            />
+          </button>
+        </Tooltip>
+        <Tooltip
+          label="Run Code"
+          hasArrow
+          fontSize="sm"
+          bg="#FFF0F3"
+          textColor="#590D22"
+        >
+          <button
+            onClick={runCode}
+            className="bg-primary-5 flex items-center text-primary-1 font-medium rounded px-3 py-0.5 mr-2"
+          >
+            <img className="h-2.5" src={runIcon} alt="run code icon" />
+            <span className="ml-2">Run</span>
+          </button>
+        </Tooltip>
+        {/* {isAuthenticated ? <Logout /> : <Login />} */}
+        {isUserPresent && (
+          <Tooltip
+            label={userInfo.email}
+            hasArrow
+            fontSize="sm"
+            bg="#FFF0F3"
+            textColor="#590D22"
+          >
+            <Avatar size="sm" name={userInfo.name} src={userInfo.picture} />
+          </Tooltip>
+        )}
+      </div>
+    </div>
+  );
 }
--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,108 +1,152 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { AuthProvider, useAuth } from "../../context/AuthProvider";
-
+import img from "../../images/img.jpg";
 function Auth() {
-	const [username, setUsername] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [confirmPassword, setConfirmPassword] = useState("");
-	const [userType, setUserType] = useState("Corporate");
-	const [token, setToken] = useState("");
-	const [isRegistering, setIsRegistering] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [userType, setUserType] = useState("Corporate");
+  const [token, setToken] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false);
 
-	const { login } = useAuth();
+  const { login } = useAuth();
 
-	const handleRegister = async () => {
-		if (password !== confirmPassword) {
-			alert("Passwords do not match");
-			return;
-		}
+  useEffect(() => {
+    console.log(isRegistering);
+  }, [isRegistering]);
+  const handleRegister = async () => {
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
 
-		try {
-			const response = await axios.post("http://localhost:3002/register", {
-				email,
-				username,
-				userType,
-				password,
-			});
-			console.log(response.data);
-			setIsRegistering(false); // After successful registration, switch to login form
-		} catch (error) {
-			console.error(error);
-		}
-	};
+    try {
+      const response = await axios.post("http://localhost:3002/register", {
+        email,
+        username,
+        userType,
+        password,
+      });
+      console.log(response.data);
+      setIsRegistering(false); // After successful registration, switch to login form
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	const handleLogin = async () => {
-		try {
-			const response = await axios.post("http://localhost:3002/login", {
-				email,
-				password,
-			});
-			localStorage.setItem("user", JSON.stringify(response.data));
-			login();
-		} catch (error) {
-			console.error(error);
-		}
-	};
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post("http://localhost:3002/login", {
+        email,
+        password,
+      });
+      localStorage.setItem("user", JSON.stringify(response.data));
+      login();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	const handleProtectedRoute = async () => {
-		try {
-			const response = await axios.get("http://localhost:3000/protected", {
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			});
-			console.log(response.data);
-		} catch (error) {
-			console.error(error);
-		}
-	};
+  const handleProtectedRoute = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/protected", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	return (
-		<div>
-			<h1>{isRegistering ? "Register" : "Login"}</h1>
-			<label>Email:</label>
-			<input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-			{isRegistering && (
-				<>
-					<label>Username:</label>
-					<input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-					<br />
-				</>
-			)}
-			<label>Password:</label>
-			<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-			<br />
-			{isRegistering && (
-				<>
-					<label>Confirm Password:</label>
-					<input
-						type="password"
-						value={confirmPassword}
-						onChange={(e) => setConfirmPassword(e.target.value)}
-					/>
-					<br />
-					<select onChange={(e) => setUserType(e.target.value)}>
-						<option value="Corporate">Corporate</option>
-						<option value="Personal">Personal</option>
-					</select>
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+      <div className="hidden sm:block">
+        <img className="w-full h-full object-cover" src={img} alt="" />
+      </div>
+      <div className="max-w-[400px] w-full mx-auto  bg-purple-standard p-8 px-8">
+        <div className="flex w-full  bg-gradient-to-r from-primary-1 via-primary-3 to-primary-5  text-transparent bg-clip-text text-3xl sm:text-7xl font-bold codeFont justify-center ">
+          <span>&#123; ..SyncLink.. &#125;</span>
+        </div>
 
-					<br />
-				</>
-			)}
-			<button onClick={isRegistering ? handleRegister : handleLogin}>
-				{isRegistering ? "Register" : "Login"}
-			</button>
+        <h2 className="text-4xl text-primary-5 font-bold text-center">
+          {isRegistering ? "Register" : "Login"}
+        </h2>
+        <div className="flex flex-col text-primary-5 py-2">
+          <label>Email:</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="rounded-lg bg-gray-700 mt-2 p-2 focus:bg-primary-5 focus:text-primary-1 focus:outline-none"
+          />
+          {isRegistering && (
+            <>
+              <div className="flex flex-col text-primary-5 py-2">
+                <label>Username:</label>
+                <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="rounded-lg bg-gray-700 mt-2 p-2  focus:bg-primary-5  focus:text-primary-1 focus:outline-none"
+                />
+              </div>
+            </>
+          )}
+        </div>
+        <div className="flex flex-col text-primary-5 py-2">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="rounded-lg bg-gray-700 mt-2 p-2  focus:bg-primary-5 focus:text-primary-1 focus:outline-none"
+          />
+        </div>
+        {isRegistering && (
+          <>
+            <div className="flex flex-col text-primary-5 py-2">
+              <label>Confirm Password:</label>
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className="rounded-lg bg-gray-700 mt-2 p-2  focus:bg-primary-5 focus:text-primary-1 focus:outline-none"
+              />
+            </div>
+            <select
+              onChange={(e) => setUserType(e.target.value)}
+              className=" mt-2 bg- rounded-lg px-4 py-2 text-primary-1 outline-none focus:bg-primary-5 focus:text-primary-1 focus:outline-none"
+            >
+              <option value="Corporate">Corporate</option>
+              <option value="Personal">Personal</option>
+            </select>
 
-			<br />
-			<button onClick={() => setIsRegistering(!isRegistering)}>
-				{isRegistering ? "Switch to Login" : "Switch to Register"}
-			</button>
-			<br />
-			<button onClick={handleProtectedRoute}>Access Protected Route</button>
-		</div>
-	);
+            <br />
+          </>
+        )}
+        <div className=" mt-5 flex flex-col items-center">
+          <button
+            onClick={isRegistering ? handleRegister : handleLogin}
+            className="w-1/2 my-5  bg-primary-4 hover:bg-primary-5  focus:outline-none  text-primary-1 font-bold py-2 px-4 shadow-md transition duration-300 ease-in-out rounded-lg"
+          >
+            {isRegistering ? "Register" : "Login"}
+          </button>
+
+          <button
+            onClick={() => setIsRegistering(!isRegistering)}
+            className="w-1/2 my-5  bg-primary-1 hover:bg-primary-2  focus:outline-none  text-primary-5 font-bold py-2 px-4 shadow-md transition duration-300 ease-in-out rounded-lg"
+          >
+            {isRegistering ? "Switch to Login" : "Switch to Register"}
+          </button>
+        </div>
+        <br />
+      </div>
+    </div>
+  );
 }
 
 export default Auth;
